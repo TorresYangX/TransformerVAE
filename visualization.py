a43cf33b1fc0f044abfcba6bac64bc9d512b7244
TorresYangX <12112729@mail.sustech.edu.cn>
2023-11-28 22:36:16 +0800

    add VAE_nvib_old

--- visualization.py
+++ visualization.py
@@ -6,6 +6,7 @@ def visualEmdResult():
     AE =  '../results/AE/KDTreeAE/EMD/emd_.npy'
     VAE = '../results/VAE/KDTreeVAE/EMD/emd_.npy'
     VAE_nvib = '../results/VAE_nvib/KDTreeVAE_nvib/EMD/emd_.npy'
+    VAE_nvib_old = '../results/VAE_nvib_old/KDTreeVAE_nvib_old/EMD/emd_.npy'
     LCSS = '../results/LCSS/KDTreeLCSS/EMD/emd_.npy'
     EDR = '../results/EDR/KDTreeEDR/EMD/emd_.npy'
     EDwP = '../results/EDwP/KDTreeEDwP/EMD/emd_.npy'
@@ -13,10 +14,11 @@ def visualEmdResult():
 
     AE_data = np.load(AE)
     VAE_data = np.load(VAE)
+    VAE_nvib_data = np.load(VAE_nvib)
+    VAE_nvib_old_data = np.load(VAE_nvib_old)
     LCSS_data = np.load(LCSS)
     EDR_data = np.load(EDR)
     EDwP_data = np.load(EDwP)
-    VAE_nvib_data = np.load(VAE_nvib)
     DTW_data = np.load(DTW)
 
     # Draw a line chart
@@ -26,21 +28,23 @@ def visualEmdResult():
 
     y21 = np.log(AE_data)
     y22 = np.log(VAE_data)
-    y23 = np.log(LCSS_data)
-    y24 = np.log(EDR_data)
-    y25 = np.log(EDwP_data)
-    y26 = np.log(VAE_nvib_data)
-    y27 = np.log(DTW_data)
+    y23 = np.log(VAE_nvib_data)
+    y24 = np.log(VAE_nvib_old_data)
+    y25 = np.log(LCSS_data)
+    y26 = np.log(EDR_data)
+    y27 = np.log(EDwP_data)
+    y28 = np.log(DTW_data)
 
 
     # Draw a line chart
     plt.plot(x, y21, 'b-', linewidth=1.5, label='AE')
     plt.plot(x, y22, 'r-', linewidth=1.5, label='VAE')
-    plt.plot(x, y26, 'm-', linewidth=1.5, label='VAE_nvib')
-    plt.plot(x, y23, 'g-.', linewidth=1.5, label='LCSS')
-    plt.plot(x, y24, 'y-', linewidth=1.5, label='EDR')
-    plt.plot(x, y25, 'c-', linewidth=1.5, label='EDwP')
-    plt.plot(x, y27, 'k-', linewidth=1.5, label='DTW')
+    plt.plot(x, y23, 'm-', linewidth=1.5, label='VAE_nvib')
+    plt.plot(x, y24, 'g-', linewidth=1.5, label='VAE_nvib_old')
+    plt.plot(x, y25, 'g-.', linewidth=1.5, label='LCSS')
+    plt.plot(x, y26, 'y-', linewidth=1.5, label='EDR')
+    plt.plot(x, y27, 'c-', linewidth=1.5, label='EDwP')
+    plt.plot(x, y28, 'k-', linewidth=1.5, label='DTW')
     
 
     # Set the x-axis label
