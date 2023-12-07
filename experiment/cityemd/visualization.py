55ae28bdb30a19f96eef4a0bb88a9140f33a5b6a
TorresYangX <12112729@mail.sustech.edu.cn>
2023-12-07 14:24:41 +0800

    visualize

--- experiment/cityemd/visualization.py
+++ experiment/cityemd/visualization.py
@@ -6,6 +6,7 @@ def visualEmdResult():
     AE =  '../results/AE/KDTreeAE/EMD/emd_.npy'
     VAE = '../results/VAE/KDTreeVAE/EMD/emd_.npy'
     VAE_nvib = '../results/VAE_nvib/KDTreeVAE_nvib/EMD/emd_.npy'
+    Transformer = '../results/Transformer/KDTreeTransformer/EMD/emd_.npy'
     LCSS = '../results/LCSS/KDTreeLCSS/EMD/emd_.npy'
     EDR = '../results/EDR/KDTreeEDR/EMD/emd_.npy'
     EDwP = '../results/EDwP/KDTreeEDwP/EMD/emd_.npy'
@@ -14,6 +15,7 @@ def visualEmdResult():
     AE_data = np.load(AE)
     VAE_data = np.load(VAE)
     VAE_nvib_data = np.load(VAE_nvib)
+    Transformer_data = np.load(Transformer)
     LCSS_data = np.load(LCSS)
     EDR_data = np.load(EDR)
     EDwP_data = np.load(EDwP)
@@ -27,6 +29,7 @@ def visualEmdResult():
     y21 = np.log(AE_data)
     y22 = np.log(VAE_data)
     y23 = np.log(VAE_nvib_data)
+    y24 = np.log(Transformer_data)
     y25 = np.log(LCSS_data)
     y26 = np.log(EDR_data)
     y27 = np.log(EDwP_data)
@@ -37,6 +40,7 @@ def visualEmdResult():
     plt.plot(x, y21, 'b-', linewidth=1.5, label='AE')
     plt.plot(x, y22, 'r-', linewidth=1.5, label='VAE')
     plt.plot(x, y23, 'm-', linewidth=1.5, label='VAE_nvib')
+    plt.plot(x, y24, 'k-', linewidth=1.5, label='Transformer')
     plt.plot(x, y25, 'g-.', linewidth=1.5, label='LCSS')
     plt.plot(x, y26, 'y-', linewidth=1.5, label='EDR')
     # plt.plot(x, y27, 'c-', linewidth=1.5, label='EDwP')
