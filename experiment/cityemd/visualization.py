07eb5f0dc8aa8692a6bd02ae9f46891a56cd849a
TorresYangX <12112729@mail.sustech.edu.cn>
2023-12-15 16:57:35 +0800

    commit_msg = """\ experiment/cityemd/visualization.py: add visualization for cityemd """

--- experiment/cityemd/visualization.py
+++ experiment/cityemd/visualization.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualEmdResult():
-    AE =  '../results/AE/KDTreeAE/EMD/emd_.npy'
-    VAE = '../results/VAE/KDTreeVAE/EMD/emd_.npy'
-    VAE_nvib = '../results/VAE_nvib/KDTreeVAE_nvib/EMD/emd_.npy'
-    Transformer = '../results/Transformer/KDTreeTransformer/EMD/emd_.npy'
-    LCSS = '../results/LCSS/KDTreeLCSS/EMD/emd_.npy'
-    EDR = '../results/EDR/KDTreeEDR/EMD/emd_.npy'
-    EDwP = '../results/EDwP/KDTreeEDwP/EMD/emd_.npy'
-    DTW = '../results/DTW/KDTreeDTW/EMD/emd_.npy'
+AE =  '../results/AE/KDTreeAE/EMD/emd_.npy'
+VAE = '../results/VAE/KDTreeVAE/EMD/emd_.npy'
+VAE_nvib = '../results/VAE_nvib/KDTreeVAE_nvib/EMD/emd_.npy'
+Transformer = '../results/Transformer/KDTreeTransformer/EMD/emd_.npy'
+LCSS = '../results/LCSS/KDTreeLCSS/EMD/emd_.npy'
+EDR = '../results/EDR/KDTreeEDR/EMD/emd_.npy'
+EDwP = '../results/EDwP/KDTreeEDwP/EMD/emd_.npy'
+DTW = '../results/DTW/KDTreeDTW/EMD/emd_.npy'
+
+def lineChart():
 
     AE_data = np.load(AE)
     VAE_data = np.load(VAE)
@@ -37,36 +38,39 @@ def visualEmdResult():
 
 
     # Draw a line chart
-    plt.plot(x, y21, 'b-', linewidth=1.5, label='AE')
-    plt.plot(x, y22, 'r-', linewidth=1.5, label='VAE')
-    plt.plot(x, y23, 'm-', linewidth=1.5, label='VAE_nvib')
-    plt.plot(x, y24, 'k-', linewidth=1.5, label='Transformer')
+    plt.plot(x, y21, 'b', linestyle='dotted', linewidth=1.5, label='AE')
+    # plt.plot(x, y22, 'r-', linewidth=1.5, label='VAE')
+    plt.plot(x, y24, 'k-.', linewidth=1.5, label='Transformer')
     plt.plot(x, y25, 'g-.', linewidth=1.5, label='LCSS')
     plt.plot(x, y26, 'y-', linewidth=1.5, label='EDR')
-    # plt.plot(x, y27, 'c-', linewidth=1.5, label='EDwP')
+    plt.plot(x, y27, 'c-.', linewidth=1.5, label='EDwP')
     # plt.plot(x, y28, 'k-', linewidth=1.5, label='DTW')
+    plt.plot(x, y23, 'r-', lw=2, label='NVAE')
     
 
-    # Set the x-axis label
-    plt.xlabel('steps')
+    # Set the x-axis label, bold font, font size 12
+    plt.xlabel('Steps', fontweight='bold', fontsize=12)
 
     # Set the y-axis label
-    plt.ylabel('log(cityEMD)')
+    plt.ylabel('log(cityEMD)', fontweight='bold', fontsize=12)
 
     # Set a title of the current axes.
-    plt.title('log(cityEMD) of differernt models')
+    plt.title('log(cityEMD) of differernt models', fontweight='bold', fontsize=12)
 
-    # show a legend on the plot
-    plt.legend()
+    # show a legend on the plot, bold font, font size 12
+    plt.legend(prop={'size': 12, 'weight': 'bold'})
 
     #save figure
     plt.savefig('../results/log(cityEMD).png')
 
     # Display a figure.
     plt.show()
+    
+     
 
 if __name__ == '__main__':
-    visualEmdResult()
+    # lineChart()
+    barChart()
 
 
 
