7b70d120de798e4aa8f4fa7e4be5de7a52717cf6
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-04 20:51:55 +0800

    change config

--- Train.py
+++ Train.py
@@ -1,105 +1,65 @@
-from baseFuncs import *
-import torch
-import torch.optim as optim
-import os
-from tqdm import trange
-from model.NVAE import TransformerNvib
+# from baseFuncs import *
+# import torch
+# import torch.optim as optim
+# import os
+# from tqdm import trange
+# from model.NVAE import TransformerNvib
 
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
-grid_num = 50
-Batch_size = 16 #B
-dropout = 0.1
-learning_rate = 0.001
-MAX_EPOCH = 30
-ACCUMULATION_STEPS = 1
-
-class Trainer:
-    def __init__(self, model, optimizer, train_loader, test_loader, trajectory_length, grid_num, epoch, ACCUMULATION_STEPS):
-        self.model = model
-        self.optimizer = optimizer
-        self.train_loader = train_loader
-        self.test_loader = test_loader
-        self.trajectory_length = trajectory_length
-        self.grid_num = grid_num
-        self.epoch = epoch
-        self.ACCUMULATION_STEPS = ACCUMULATION_STEPS
-    
-    def training(self):
-        train_losses_value = 0
-        for idx, x in enumerate(self.train_loader):
-            x = x[0].transpose(0,1).to(device)
-            eos = torch.full((1, x.shape[1]), self.grid_num*self.grid_num).to(device)
-            sos = torch.full((1, x.shape[1]), self.grid_num*self.grid_num+1).to(device)
-            src = torch.cat([x, eos], dim=0)
-            tgt = torch.cat([sos, x], dim=0)
-
-            src_key_padding_mask = torch.zeros((x.shape[1], self.trajectory_length + 1), dtype=torch.bool).to(device)
-            tgt_key_padding_mask = torch.zeros((x.shape[1], self.trajectory_length + 1), dtype=torch.bool).to(device)
-
-            train_outputs_dict = self.model(
-                src,
-                tgt,
-                src_key_padding_mask=src_key_padding_mask,
-                tgt_key_padding_mask=tgt_key_padding_mask,
-            )
-            train_losses = self.model.loss(**train_outputs_dict, targets=tgt, epoch=self.epoch)
-            (train_losses["Loss"] / self.ACCUMULATION_STEPS).backward()
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.1)
-            if ((idx + 1) % self.ACCUMULATION_STEPS == 0) or (idx + 1 == len(self.train_loader)):
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-            train_losses_value += train_losses["Loss"].item()
-        return train_losses_value / len(self.train_loader.dataset)
-
-    def evaluation(self):
-        test_losses_value = 0
-        for _, x in enumerate(self.test_loader):
-            with torch.no_grad():
-                x = x[0].transpose(0,1).to(device)
-                eos = torch.full((1, x.shape[1]), self.grid_num*self.grid_num).to(device)
-                sos = torch.full((1, x.shape[1]), self.grid_num*self.grid_num+1).to(device)
-                src = torch.cat([x, eos], dim=0)
-                tgt = torch.cat([sos, x], dim=0)
-
-                src_key_padding_mask = torch.zeros((x.shape[1], self.trajectory_length + 1), dtype=torch.bool).to(device)
-                tgt_key_padding_mask = torch.zeros((x.shape[1], self.trajectory_length + 1), dtype=torch.bool).to(device)
-
-                test_outputs_dict = self.model(
-                    src,
-                    tgt,
-                    src_key_padding_mask=src_key_padding_mask,
-                    tgt_key_padding_mask=tgt_key_padding_mask,
-                )
-                test_losses = self.model.loss(**test_outputs_dict, targets=tgt, epoch=self.epoch)
-                test_losses_value += test_losses["Loss"].item()
-        return test_losses_value / len(self.test_loader.dataset)
+# grid_num = 50
+# Batch_size = 16 #B
+# dropout = 0.1
+# learning_rate = 0.001
+# MAX_EPOCH = 30
+# ACCUMULATION_STEPS = 1
     
 
-def trainModel(trainFilePath, modelSavePath, trainlogPath, trajectory_length):
-    x = constructTrainingData(trainFilePath, Batch_size)
-    dataSet = torch.utils.data.TensorDataset(torch.from_numpy(x))
-    val_size = int(0.2 * len(dataSet))
-    model = TransformerNvib().to(device)
-    optimizer = optim.Adam(model.parameters(),lr=learning_rate)
-    logger = get_logger(trainlogPath)
-    train_loss_list = []
-    test_loss_list = []
-    print("Start training...")
-    for epoch in trange(MAX_EPOCH):
-        train_dataset, test_dataset = torch.utils.data.random_split(dataSet, [len(dataSet) - val_size, val_size])
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=Batch_size)
-        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=Batch_size)
-        trainer = Trainer(model, optimizer, train_loader, test_loader, trajectory_length, grid_num, epoch, ACCUMULATION_STEPS)
-        train_loss = trainer.training()
-        test_loss = trainer.evaluation()
-        if epoch == 0 or train_loss < min(train_loss_list):
-            torch.save(model, modelSavePath)
-        logger.info('Epoch:[{}/{}]\t Train Loss={:.4f}\t Test Loss={:.4f}'.format(epoch+1 , MAX_EPOCH, train_loss, test_loss ))
-        train_loss_list.append(train_loss)
-        test_loss_list.append(test_loss)
-    print("End training...")
-    plot_loss(train_loss_list, test_loss_list)
+# def trainModel(trainFilePath, modelSavePath, trainlogPath, trajectory_length):
+#     x = constructTrainingData(trainFilePath, Batch_size)
+#     dataSet = torch.utils.data.TensorDataset(torch.from_numpy(x))
+#     val_size = int(0.2 * len(dataSet))
+#     model = TransformerNvib().to(device)
+#     optimizer = optim.Adam(model.parameters(),lr=learning_rate)
+#     logger = get_logger(trainlogPath)
+#     train_loss_list = []
+#     test_loss_list = []
+#     print("Start training...")
+#     for epoch in trange(MAX_EPOCH):
+#         train_dataset, test_dataset = torch.utils.data.random_split(dataSet, [len(dataSet) - val_size, val_size])
+#         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=Batch_size)
+#         test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=Batch_size)
+#         trainer = Trainer(model, optimizer, train_loader, test_loader, trajectory_length, grid_num, epoch, ACCUMULATION_STEPS)
+#         train_loss = trainer.training()
+#         test_loss = trainer.evaluation()
+#         if epoch == 0 or train_loss < min(train_loss_list):
+#             torch.save(model, modelSavePath)
+#         logger.info('Epoch:[{}/{}]\t Train Loss={:.4f}\t Test Loss={:.4f}'.format(epoch+1 , MAX_EPOCH, train_loss, test_loss ))
+#         train_loss_list.append(train_loss)
+#         test_loss_list.append(test_loss)
+#     print("End training...")
+#     plot_loss(train_loss_list, test_loss_list)
+
+import argparse
+from model_config import ModelConfig
+from dataset_config import DatasetConfig
+from model.NVAE_trainer import Trainer
+
+def parse_args():
+    # dont set default value here! -- it will incorrectly overwrite the values in config.py.
+    # config.py is the correct place for default values.
+    
+    parser = argparse.ArgumentParser(description = "NVAE/train.py")
+    parser.add_argument('--dataset', type = str, help = '')
+
+    args = parser.parse_args()
+    return dict(filter(lambda kv: kv[1] is not None, vars(args).items()))
+
+if __name__ == '__main__':
+    DatasetConfig.update(parse_args())
+    trainer = Trainer()
+    # trainer.train()
+    trainer.encode('test')
--- __init__.py
+++ __init__.py
@@ -3,27 +3,27 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 import numpy as np
 import pandas as pd
-from config import Config
+from dataset_config import DatasetConfig
 import matplotlib.pyplot as plt
 from utils.pkl2csv import pkl2csv
-from utils import preprocessing_proto
+from utils import preprocessing_porto
 from utils import preprocessing_geolife
-from utils.renumberTrajectories import renumberTrajectories
+from utils.seq2multi_row import seq2multi_row
 
 
-Config.dataset = 'geolife'
-Config.post_value_updates()
-Config.to_str()
-# preprocessing_proto.clean_and_output_data()
-# preprocessing_proto.generate_lonlat_data()
-# preprocessing_proto.generate_grid_data()
+DatasetConfig.dataset = 'geolife'
+DatasetConfig.post_value_updates()
+# preprocessing_porto.clean_and_output_data()
+# preprocessing_porto.generate_intepolation_data()
+# preprocessing_porto.generate_lonlat_data()
+# preprocessing_porto.generate_grid_data()
 
 # preprocessing_geolife.clean_and_output_data()
+# preprocessing_geolife.generate_intepolation_data()
 # preprocessing_geolife.generate_lonlat_data()
 # preprocessing_geolife.generate_grid_data()
 
+# seq2multi_row(Config.lonlat_ground_file, Config.lonlat_ground_file.replace('.pkl', '_multirow.csv'))
 
-pkl2csv(Config.lonlat_ground_file, Config.lonlat_ground_file[:-4] + '.csv')
-renumberTrajectories(Config.lonlat_ground_file[:-4] + '.csv', Config.lonlat_ground_file[:-4] + '_renumbered.csv')
-
-
+# data = pd.read_pickle(Config.grid_ground_file)
+# print(data.head())
