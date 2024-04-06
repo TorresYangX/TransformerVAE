b63bd6f094f7f2e52389888e757451df099ff211
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-06 17:11:04 +0800

    VAE_trainer.py

--- baseline/DeepModels/VAE.py
+++ baseline/DeepModels/VAE.py
@@ -1,19 +1,29 @@
+import os
+import time
 import torch
 import torch.nn as nn
+from utils import tool_funcs
+from datetime import datetime
+from model_config import ModelConfig
+from dataset_config import DatasetConfig
+from utils.dataloader import read_traj_dataset
+from torch.utils.data.dataloader import DataLoader
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+import logging
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 
 class VAE(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, latent_dim, vocab_size, BATCH_SIZE, trajectory_length):
+    def __init__(self):
         super(VAE, self).__init__()
-        self.embedding_dim = embedding_dim
-        self.trajectory_length = trajectory_length
-        self.hidden_dim = hidden_dim
-        self.latent_dim = latent_dim
-        self.vocab_size = vocab_size
-        self.BATCH_SIZE = BATCH_SIZE
+        self.embedding_dim = ModelConfig.VAE.embedding_dim
+        self.trajectory_length = ModelConfig.VAE.traj_len
+        self.hidden_dim = ModelConfig.VAE.hidden_dim
+        self.latent_dim = ModelConfig.VAE.latent_dim
+        self.vocab_size = ModelConfig.VAE.vocab_size
+        self.BATCH_SIZE = ModelConfig.VAE.BATCH_SIZE
         
         self.embedding = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embedding_dim)
         self.encoder_lstm = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.hidden_dim, batch_first=True)
@@ -25,8 +35,8 @@ class VAE(nn.Module):
         self.outfc = nn.Linear(self.embedding_dim, self.vocab_size)
         self.softmax = nn.Softmax(dim=2)
 
-        self.hidden_init = (torch.autograd.Variable(torch.zeros(1, self.BATCH_SIZE, self.hidden_dim).to(device)),
-                            torch.autograd.Variable(torch.zeros(1, self.BATCH_SIZE, self.hidden_dim).to(device)))
+        self.hidden_init = (torch.autograd.Variable(torch.zeros(1, self.BATCH_SIZE, self.hidden_dim).to(ModelConfig.device)),
+                            torch.autograd.Variable(torch.zeros(1, self.BATCH_SIZE, self.hidden_dim).to(ModelConfig.device)))
 
         self.softsign = nn.Softsign()
 
@@ -74,6 +84,98 @@ class VAE(nn.Module):
             "mu": mu,
             "logvar": logvar,
         }
+        
+        
+class VAE_Trainer:
+    def __init__(self):
+        super(VAE_Trainer, self).__init__()
+        
+        self.model = VAE().to(ModelConfig.device)
+        self.checkpoint_file = '{}/{}_VAE_best.pt'.format(ModelConfig.VAE.checkpoint_dir, DatasetConfig.dataset)
+        
+        
+    def train(self):
+        training_starttime = time.time()
+        train_dataset = read_traj_dataset(DatasetConfig.grid_total_file)
+        train_dataloader = DataLoader(train_dataset, 
+                                    batch_size=ModelConfig.VAE.BATCH_SIZE, 
+                                    shuffle=False, 
+                                    num_workers=0, 
+                                    drop_last=True)
+        
+        training_gpu_usage = training_ram_usage = 0.0
+        logging.info("[Training] START! timestamp={}".format(datetime.fromtimestamp(training_starttime)))
+        torch.autograd.set_detect_anomaly(True)
+        
+        optimizer = torch.optim.Adam(self.model.parameters(), lr = ModelConfig.VAE.learning_rate, weight_decay = 0.0001)
+        
+        best_loss_train = 100000
+        best_epoch = 0
+        bad_counter = 0
+        bad_patience = ModelConfig.VAE.training_bad_patience
+        
+        for i_ep in range(ModelConfig.VAE.MAX_EPOCH):
+            _time_ep = time.time()
+            loss_ep = []
+            train_gpu = []
+            train_ram = []
+            
+            self.model.train()
+            
+            _time_batch_start = time.time()
+            for i_batch, batch in enumerate(train_dataloader):
+                print("batch: ", i_batch)
+                optimizer.zero_grad()
+                
+                train_dict = self.model(batch.to(ModelConfig.device))
+                train_loss = self.model.loss_fn(targets=batch.to(ModelConfig.device), **train_dict)
+                
+                train_loss['Loss'].backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                
+                loss_ep.append(train_loss['Loss'].item())
+                train_gpu.append(tool_funcs.GPUInfo.mem()[0])
+                train_ram.append(tool_funcs.RAMInfo.mem())
+                
+                if i_batch % 100 == 0 and i_batch:
+                    logging.debug("[Training] ep-batch={}-{}, loss={:.3f}, @={:.3f}, gpu={}, ram={}" \
+                            .format(i_ep, i_batch, train_loss['Loss'].item(), time.time() - _time_batch_start,
+                                    tool_funcs.GPUInfo.mem(), tool_funcs.RAMInfo.mem()))
+                
+            loss_ep_avg = tool_funcs.mean(loss_ep)
+            logging.info("[Training] ep={}: avg_loss={:.3f}, @={:.3f}/{:.3f}, gpu={}, ram={}" \
+                    .format(i_ep, loss_ep_avg, time.time() - _time_ep, time.time() - training_starttime,
+                    tool_funcs.GPUInfo.mem(), tool_funcs.RAMInfo.mem()))
+            
+            training_gpu_usage = tool_funcs.mean(train_gpu)
+            training_ram_usage = tool_funcs.mean(train_ram)
+            
+            # early stopping
+            if loss_ep_avg < best_loss_train:
+                best_epoch = i_ep
+                best_loss_train = loss_ep_avg
+                bad_counter = 0
+                self.save_checkpoint()
+            else:
+                bad_counter += 1
+                
+            if bad_counter == bad_patience or (i_ep + 1) == ModelConfig.VAE.MAX_EPOCH:
+                logging.info("[Training] END! @={}, best_epoch={}, best_loss_train={:.6f}" \
+                            .format(time.time()-training_starttime, best_epoch, best_loss_train))
+                break
+        
+        return {'enc_train_time': time.time()-training_starttime, \
+            'enc_train_gpu': training_gpu_usage, \
+            'enc_train_ram': training_ram_usage}
+                
+                
+                
+    def save_checkpoint(self):
+        torch.save({'model_state_dict': self.model.state_dict(),},
+                    self.checkpoint_file)
+        return 
+        
 
 
 
