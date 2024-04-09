bb69ddcf33ebf3df77c142b97ab52ce05567eb5d
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-09 18:02:53 +0800

    remove useless funcs

--- utils/tool_funcs.py
+++ utils/tool_funcs.py
@@ -1,10 +1,9 @@
 import os
 import psutil
-import logging
 import numpy as np
 import pandas as pd
 from pynvml import *
-import matplotlib.pyplot as plt
+from model_config import ModelConfig
 
 nvmlInit() # need initializztion here
 
@@ -12,70 +11,21 @@ def mean(x):
     if x == []:
         return 0.0
     return sum(x) / len(x)
-
-
-def constructTrainingData(filePath, BATCH_SIZE):
-    x = []
-    for file in os.listdir(filePath):
-        data = np.load(filePath + file)
-        x.extend(data)
-    x = np.array(x)
-    resid = (x.shape[0] // BATCH_SIZE) * BATCH_SIZE
-    x = x[:resid, :, :]
-    x = x[:, :, 0]
-    return x
-
-def constructSingleData(filePath, file, BATCH_SIZE):
-    data = np.load(filePath + file)
-    x = np.array(data)
-    resid = (x.shape[0] // BATCH_SIZE) * BATCH_SIZE
-    x = x[:resid, :, :]
-    return x[:, :,0]
-        
-def parameteroutput(data, file):
-    directory = os.path.dirname(file)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    para = pd.DataFrame(data)
-    with open(file, mode = 'w') as f:
-        para.to_csv(f, index = False, header = None)
-
-def get_logger(filename, verbosity=1, name=None):
-    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
-    formatter = logging.Formatter(
-        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
-    )
-    logger = logging.getLogger(name)
-    logger.setLevel(level_dict[verbosity])
- 
-    fh = logging.FileHandler(filename, "w")
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
- 
-    sh = logging.StreamHandler()
-    sh.setFormatter(formatter)
-    logger.addHandler(sh)
-    return logger
-
-def plot_loss(train_loss_list, test_loss_list, args):
-    x=[i for i in range(len(train_loss_list))]
-    figure = plt.figure(figsize=(20, 8), dpi=80)
-    plt.plot(x,train_loss_list,label='train_losses')
-    plt.plot(x,test_loss_list,label='test_losses')
-    plt.xlabel("iterations",fontsize=15)
-    plt.ylabel("loss",fontsize=15)
-    plt.legend()
-    plt.grid()
-    if not args.SSM_KNN:
-        plt.savefig('../results/{}/loss_figure.png'.format(args.MODEL))
-    else:
-        plt.savefig('../SSM_KNN/{}/loss_figure.png'.format(args.MODEL))
-    plt.show()
     
 
 def pkl2csv(pkl_file, csv_file):
     df = pd.read_pickle(pkl_file)
     df.to_csv(csv_file, index=False, header=None)
+    
+def get_real_ground(ground_file, test_file, _r_ground_file_path):
+    test_file_len = pd.read_pickle(test_file).shape[0] - pd.read_pickle(test_file).shape[0] % ModelConfig.NVAE.BATCH_SIZE 
+    ground_data = pd.read_pickle(ground_file)
+    ground_data.columns = ['TAXI_ID', 'wgs_seq', 'timestamp']
+    ground_data = ground_data.iloc[:-(ground_data.shape[0] % test_file_len)]
+    ground_data = ground_data.reset_index(drop=True)
+    ground_data.to_pickle(_r_ground_file_path)
+    return 
+    
     
     
 class GPUInfo:
