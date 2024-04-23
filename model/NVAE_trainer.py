399a6ca029335c7a0b53c02beb607d34670d4bdd
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-23 22:27:31 +0800

    ds_rate NVAE

--- model/NVAE_trainer.py
+++ model/NVAE_trainer.py
@@ -174,10 +174,17 @@ class Trainer:
             return
         
         db_size = [20,40,60,80,100] # dataset_size: 20K, 40K, 60K, 80K, 100K
-        ds_rate = [] # down-sampling rate: 
+        ds_rate = [0.1, 0.2] # down-sampling rate: 0.1, 0.2
         dt_rate = [] # distort rate: 
         for n_db in db_size:
             dataset_name = 'db_{}K'.format(n_db)
+            encode_single(dataset_name, 'total')
+            encode_single(dataset_name, 'ground')
+            encode_single(dataset_name, 'test')
+            logging.info('[{} Encode]end.'.format(dataset_name))
+        
+        for v_ds in ds_rate:
+            dataset_name = 'ds_{}'.format(v_ds)
             encode_single(dataset_name, 'total')
             encode_single(dataset_name, 'ground')
             encode_single(dataset_name, 'test')
