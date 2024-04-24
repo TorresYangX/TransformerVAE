34ccaf7a4106c47a2a4e90a4d28cdbe84d85dce8
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-25 00:16:40 +0800

    for general data

--- utils/seq2multi_row.py
+++ utils/seq2multi_row.py
@@ -1,21 +1,22 @@
 import pandas as pd
+from tqdm import tqdm
 from model_config import ModelConfig
 
+ID = -1
 
 def seq2multi_row(file_path, OutputFile):
+    
     def convert_multi_row(row):
-        return [[row['TAXI_ID'], coord[0], coord[1], pd.to_datetime(row['timestamp'])] for coord in row['wgs_seq']]
+        global ID
+        ID += 1
+        return [[ID, coord[0], coord[1], pd.to_datetime(row['timestamp'])] for coord in row['wgs_seq']]
     
     
     df = pd.read_pickle(file_path)
-    df.columns = ['TAXI_ID', 'wgs_seq', 'timestamp']
+    totalID = df.shape[0]
     df = df[['TAXI_ID', 'wgs_seq', 'timestamp']]
     df = df.apply(convert_multi_row, axis=1).explode().tolist()
     df = pd.DataFrame(df, columns=['TAXI_ID', 'lon', 'lat', 'timestamp'])
-    totalID = int(df.shape[0]/ModelConfig.NVAE.traj_len)
-    for i in range(totalID):
-        for j in range(ModelConfig.NVAE.traj_len):
-            df.loc[i*ModelConfig.NVAE.traj_len+j, 'TAXI_ID'] = i
     # write out data
     with open(OutputFile, mode='w') as f:
         df.to_csv(OutputFile, header=None, index=False)
