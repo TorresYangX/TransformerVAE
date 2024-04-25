7d5928b1df1f5707127625bd6dbd91001b09edca
TorresYangX <12112729@mail.sustech.edu.cn>
2024-04-25 22:46:56 +0800

    new visualization

--- utils/visualization.py
+++ utils/visualization.py
@@ -2,204 +2,75 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from model_config import ModelConfig
+from dataset_config import DatasetConfig
 
 db_size = [20,40,60,80,100]
-ds_rate = [0.0,0.1,0.2]
+ds_rate = [0.1,0.2,0.3,0.4,0.5]
 
+
+def plot(x_list, y_dict, x_label, y_label, title):
+    plt.figure()
+    for key in y_dict.keys():
+        plt.plot(x_list, y_dict[key], 'o-.', label=key)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.savefig(ModelConfig.NVAE.checkpoint_dir[:-4]+'visualization/' + title + '.png')
+    plt.show()
 
 def emd_chart(var_tp):
     NVAE_emd_folder = ModelConfig.NVAE.checkpoint_dir + '/emd'
     t2vec_emds_folder = ModelConfig.t2vec.checkpoint_dir + '/emd'
     NVAE_emds = []
     t2vec_emds = []
-    for n_db in db_size:
-        dataset_name = var_tp + str(n_db) + 'K'
+    type_dict = {
+        'db_': (db_size, 'K', 'database size(K)'),
+        'ds_': (ds_rate, '', 'downsampling rate')
+    }
+    for n_db in type_dict[var_tp][0]:
+        dataset_name = var_tp + str(n_db) + type_dict[var_tp][1]
         emd_NVAE = np.load(NVAE_emd_folder + '/emd_{}.npy'.format(dataset_name))
         emd_t2vec = np.load(t2vec_emds_folder + '/emd_{}.npy'.format(dataset_name))
         NVAE_emds.append(math.log(emd_NVAE))
         t2vec_emds.append(math.log(emd_t2vec))
+    plot_dict = {'NVAE': NVAE_emds, 't2vec': t2vec_emds}
+    plot(type_dict[var_tp][0], plot_dict, type_dict[var_tp][2], 'log(EMD)', 'emd_chart_' + var_tp)
     
-    # 显示点并且连接到线用虚线
-    plt.plot(db_size, NVAE_emds, 'ro-', label='NVAE')
-    plt.plot(db_size, t2vec_emds, 'bo-', label='t2vec')
     
-    plt.xlabel('database size(K)')
-    plt.ylabel('EMD')
-    plt.legend()
-    plt.show()
-    plt.savefig('emd_chart_db.png')
-
-def NMD_chart(var_tp):
+    
+    
+def yao_chart(metric_name, var_tp):
     NVAE_yao_folder = ModelConfig.NVAE.checkpoint_dir + '/yao'
     t2vec_yao_folder = ModelConfig.t2vec.checkpoint_dir + '/yao'
-    NVAE_NMDs = []
-    t2vec_NMDs = []
-    # for n_db in db_size:
-    #     dataset_name = var_tp + str(n_db) + 'K'
-    #     NMD_NVAE = 0
-    #     NMD_t2vec = 0
-    #     with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'mass similarity(NMD)' in line:
-    #                 NMD_NVAE = float(line.split(':')[1])
-    #                 break
-    #     with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'mass similarity(NMD)' in line:
-    #                 NMD_t2vec = float(line.split(':')[1])
-    #                 break
-    #     NVAE_NMDs.append(NMD_NVAE)
-    #     t2vec_NMDs.append(NMD_t2vec)
+    NVAE_ = []
+    t2vec_ = []
+    type_dict = {
+        'db_': (db_size, 'K', 'database size(K)'),
+        'ds_': (ds_rate, '', 'downsampling rate')
+    }
+    metric_dict = {
+        'NMD': 'mass similarity(NMD)',
+        'NMA': 'incluveness(NMA)',
+        'RRNSA': 'structure similarity(RRNSA)'
+    }
     
-    # # 显示点并且连接到线用虚线, 点粗一点
-    # plt.plot(db_size, NVAE_NMDs, 'ro-.', label='NVAE')
-    # plt.plot(db_size, t2vec_NMDs, 'bo-.', label='t2vec')
-    # plt.xlabel('database size(K)')
-    # plt.ylabel('NMD')
-    # plt.legend()
-    # plt.show()
-    # plt.savefig('NMD_chart_db.png')
-    
-    for v_ds in ds_rate:
-        NMD_NVAE = 0
-        NMD_t2vec = 0
-        dataset_name = var_tp + str(v_ds)
+    for n_db in type_dict[var_tp][0]:
+        dataset_name = var_tp + str(n_db) + type_dict[var_tp][1]
         with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
             for line in f:
-                if 'mass similarity(NMD)' in line:
-                    NMD_NVAE = float(line.split(':')[1])
+                if metric_dict[metric_name] in line:
+                    value = float(line.split(':')[1])
+                    NVAE_.append(value)
                     break
         with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
             for line in f:
-                if 'mass similarity(NMD)' in line:
-                    NMD_t2vec = float(line.split(':')[1])
+                if metric_dict[metric_name] in line:
+                    value = float(line.split(':')[1])
+                    t2vec_.append(value)
                     break
-        NVAE_NMDs.append(NMD_NVAE)
-        t2vec_NMDs.append(NMD_t2vec)
-        
-    # 显示点并且连接到线用虚线, 点粗一点
-    plt.plot(ds_rate, NVAE_NMDs, 'ro-.', label='NVAE')
-    plt.plot(ds_rate, t2vec_NMDs, 'bo-.', label='t2vec')
-    plt.xlabel('downsampling rate')
-    plt.ylabel('NMD')
-    plt.legend()
-    plt.show()
-    plt.savefig('NMD_chart_ds.png')
-    
-def NMA_chart(var_tp):
-    NVAE_yao_folder = ModelConfig.NVAE.checkpoint_dir + '/yao'
-    t2vec_yao_folder = ModelConfig.t2vec.checkpoint_dir + '/yao'
-    NVAE_NMAs = []
-    t2vec_NMAs = []
-    # for n_db in db_size:
-    #     dataset_name = var_tp + str(n_db) + 'K'
-    #     NMA_NVAE = 0
-    #     NMA_t2vec = 0
-    #     with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'incluveness(NMA)' in line:
-    #                 NMA_NVAE = float(line.split(':')[1])
-    #                 break
-    #     with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'incluveness(NMA)' in line:
-    #                 NMA_t2vec = float(line.split(':')[1])
-    #                 break
-    #     NVAE_NMAs.append(NMA_NVAE)
-    #     t2vec_NMAs.append(NMA_t2vec)
-    
-    # # 显示点并且连接到线用虚线, 点粗一点
-    # plt.plot(db_size, NVAE_NMAs, 'ro-.', label='NVAE')
-    # plt.plot(db_size, t2vec_NMAs, 'bo-.', label='t2vec')
-    # plt.xlabel('database size(K)')
-    # plt.ylabel('NMA')
-    # plt.legend()
-    # plt.show()
-    # plt.savefig('NMA_chart_db.png')
-    
-    for v_ds in ds_rate:
-        NMA_NVAE = 0
-        NMA_t2vec = 0
-        dataset_name = var_tp + str(v_ds)
-        with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-            for line in f:
-                if 'incluveness(NMA)' in line:
-                    NMA_NVAE = float(line.split(':')[1])
-                    break
-        with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-            for line in f:
-                if 'incluveness(NMA)' in line:
-                    NMA_t2vec = float(line.split(':')[1])
-                    break
-        NVAE_NMAs.append(NMA_NVAE)
-        t2vec_NMAs.append(NMA_t2vec)
-        
-    # 显示点并且连接到线用虚线, 点粗一点
-    plt.plot(ds_rate, NVAE_NMAs, 'ro-.', label='NVAE')
-    plt.plot(ds_rate, t2vec_NMAs, 'bo-.', label='t2vec')
-    plt.xlabel('downsampling rate')
-    plt.ylabel('NMA')
-    plt.legend()
-    plt.show()
-    plt.savefig('NMA_chart_ds.png')
-    
-    
-def RRNSA_chart(var_tp):
-    NVAE_yao_folder = ModelConfig.NVAE.checkpoint_dir + '/yao'
-    t2vec_yao_folder = ModelConfig.t2vec.checkpoint_dir + '/yao'
-    NVAE_RRNSAs = []
-    t2vec_RRNSAs = []
-    # for n_db in db_size:
-    #     dataset_name = var_tp + str(n_db) + 'K'
-    #     RRNSA_NVAE = 0
-    #     RRNSA_t2vec = 0
-    #     with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'structure similarity(RRNSA)' in line:
-    #                 RRNSA_NVAE = float(line.split(':')[1])
-    #                 break
-    #     with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-    #         for line in f:
-    #             if 'structure similarity(RRNSA)' in line:
-    #                 RRNSA_t2vec = float(line.split(':')[1])
-    #                 break
-    #     NVAE_RRNSAs.append(RRNSA_NVAE)
-    #     t2vec_RRNSAs.append(RRNSA_t2vec)
-    
-    # # 显示点并且连接到线用虚线, 点粗一点
-    # plt.plot(db_size, NVAE_RRNSAs, 'ro-.', label='NVAE')
-    # plt.plot(db_size, t2vec_RRNSAs, 'bo-.', label='t2vec')
-    # plt.xlabel('database size(K)')
-    # plt.ylabel('RRNSA')
-    # plt.legend()
-    # plt.show()
-    # plt.savefig('RRNSA_chart_db.png')
-    
-    for v_ds in ds_rate:
-        RRNSA_NVAE = 0
-        RRNSA_t2vec = 0
-        dataset_name = var_tp + str(v_ds)
-        with open(NVAE_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-            for line in f:
-                if 'structure similarity(RRNSA)' in line:
-                    RRNSA_NVAE = float(line.split(':')[1])
-                    break
-        with open(t2vec_yao_folder + '/MD_NMD_{}.csv'.format(dataset_name), 'r') as f:
-            for line in f:
-                if 'structure similarity(RRNSA)' in line:
-                    RRNSA_t2vec = float(line.split(':')[1])
-                    break
-        NVAE_RRNSAs.append(RRNSA_NVAE)
-        t2vec_RRNSAs.append(RRNSA_t2vec)
-        
-    # 显示点并且连接到线用虚线, 点粗一点
-    plt.plot(ds_rate, NVAE_RRNSAs, 'ro-.', label='NVAE')
-    plt.plot(ds_rate, t2vec_RRNSAs, 'bo-.', label='t2vec')
-    plt.xlabel('downsampling rate')
-    plt.ylabel('RRNSA')
-    plt.legend()
-    plt.show()
-    plt.savefig('RRNSA_chart_ds.png')
+    plot_dict = {'NVAE': NVAE_, 't2vec': t2vec_}
+    plot(type_dict[var_tp][0], plot_dict, type_dict[var_tp][2], metric_name, metric_name + '_chart_' + var_tp)
+     
     
     
     
