52ef44ef035d7cd9f28abfec347f569e8134a585
TorresYangX <12112729@mail.sustech.edu.cn>
2024-05-04 21:10:57 +0800

    init

--- __init__.py
+++ __init__.py
@@ -4,29 +4,23 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import numpy as np
 import pandas as pd
 from dataset_config import DatasetConfig
+from model_config import ModelConfig
 from utils import preprocessing_porto
 from utils import preprocessing_geolife
 from utils.seq2multi_row import seq2multi_row
+from utils import visualization
 
 
 DatasetConfig.dataset = 'porto'
 DatasetConfig.post_value_updates()
-# preprocessing_porto.clean_and_output_data()
-# preprocessing_porto.generate_intepolation_data()
-# preprocessing_porto.generate_lonlat_data()
-# preprocessing_porto.generate_grid_data()
 
-# preprocessing_geolife.clean_and_output_data()
-# preprocessing_geolife.generate_intepolation_data()
-# preprocessing_geolife.generate_lonlat_data()
-# preprocessing_geolife.generate_grid_data()
+# train_generator = preprocessing_porto.downsampling_dataset_generator(0.5)
+# train_generator.generate()
 
-# seq2multi_row(DatasetConfig.lonlat_ground_file, DatasetConfig.lonlat_ground_file.replace('.pkl', '_multirow.csv'))
+data = pd.read_pickle(DatasetConfig.dataset_folder+'/db_20K/lonlat/porto_test.pkl')
+print(data.head())
+# seq2multi_row(DatasetConfig.dataset_folder+'/db_20K/porto.pkl', DatasetConfig.dataset_folder+'/db_20K/porto.csv')
 
-# data = pd.read_pickle(DatasetConfig.lonlat_ground_file)
-# print(data.head())
+# visualization.emd_chart('db_')
+# visualization.yao_chart('NMD', 'ds_')
 
-# seq2multi_row('exp\porto\\t2vec\emd\\retrieve_trajs.csv', 'exp\porto\\t2vec\emd\\retrieve_trajs_multirow.csv')
-
-train_generator = preprocessing_porto.train_dataset_generator()
-train_generator.generate()
